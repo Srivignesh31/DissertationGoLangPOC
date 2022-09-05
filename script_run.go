6444dd384016f332c2fc8b5b1b84a01b4a344975
Srivignesh Nagarajan <44160548+Srivignesh31@users.noreply.github.com>
2022-09-05 11:25:44 +0100

    Update script_run.go

--- script_run.go
+++ script_run.go
@@ -4,7 +4,8 @@ import (
     "os/exec"
 )
 func main() {
-  cmdStr := "docker run -itd srivignesh/kali_gui:v2"
+//     run a kali linux vm which has remote access
+  cmdStr := "docker run -p 25900:5900 -p 25901:5901 --name kali -itd srivignesh/kali_gui:v2"
   out, _ := exec.Command("/bin/sh", "-c", cmdStr).Output()  
   fmt.Printf("%s", out)
 }   
